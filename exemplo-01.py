7cf853917756ca52df4192e63715ef5f55311762
Ti Force <tiforce77@gmail.com>
2026-05-20 13:12:02 -0300

    Atualizacao exemplo-01

--- exemplo-01.py
+++ exemplo-01.py
@@ -1,1 +1,9 @@
-print("Segunda adicao ao projeto")
+#  Crie um programa que peça dois números e some o resultado.
+
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print(f"A soma de {numero1} e {numero2} é igual a {numero1 + numero2}")
+
+print(type(numero1))
